65415a5bcf7974679841cab1d0e501f6ed9a9b68
Om Nigam <om2021064@akgec.ac.in>
2024-06-30 02:06:45 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text>Hello how are sdfghjklrtyuiohjklyou</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
